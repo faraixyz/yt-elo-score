3df7ef7b8df3ab649038cb001e79c1b0df5f23ce
Farai Gandiya <gandiyafarai@gmail.com>
2023-05-01 15:56:22 +0200

    js: collected data from api

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 require('dotenv').config()
+const {readFileSync, writeFileSync} = require("fs")
 const {google} = require("googleapis")
-const csv = require("csv-parse")
+const {parse} = require("csv-parse/sync")
 
 const yt = google.youtube({
     version: 'v3',
     auth: process.env.YT_API_KEY
 })
 
-async function test () {
-    const res = await yt.channels.list({
-        part: 'topicDetails,snippet',
-        id: ["UCyEA3vUnlpg0xzkECEq1rOA", "UC4a9LfdavRlVMaSSWFdIciA"]
-    })
 
-    console.log(res.data.items)
+async function main () {
+    const subsFile = readFileSync("subscriptions.csv", {encoding: "utf-8"})
+    const subs = parse(subsFile, {
+        skipEmptyLines: true,
+        from: 2
+    }).map(x => x[0])
+    const subsinfo = []
+
+    for (let i=0; i < subs.length; i+=50) {
+        const res = await yt.channels.list({
+            part: "snippet,topicDetails",
+            id: subs.slice(i, i+50).join(','),
+            maxResults: 50, 
+        })
+
+        for (const sub of res.data.items) {
+            const subdata = {
+                title: sub.snippet.title,
+                description: sub.snippet.description,
+                id: sub.id
+            }
+
+            if (sub.snippet.thumbnails) {
+                subdata.thumbnail = sub.snippet.thumbnails["default"]
+            }
+
+            if (sub.topicDetails) {
+                subdata.topics = sub.topicDetails.topicCategories
+            }
+
+            subsinfo.push(subdata)
+        }
+
+    }
+    console.log(subsinfo.length)
+    writeFileSync("subinfo.json", JSON.stringify(subsinfo, null, 4))
 }
 
-test()
+main()
